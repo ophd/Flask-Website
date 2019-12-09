b4db08ecd3eab1774f66aca42207c6908f572e29
Olivier Daigle <ophd@pm.me>
2019-12-09 18:22:51 -0500

    Completed Part 3 of the tutorial.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Flask-Website
-My personal website designed using Flask.
+I'm currently learning Flask via Corey Shaefer's excellent tutorial series on Flask with the intention of turning this web app into my personal website.
---
+++ forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    '''
+    Create a user registration form class
+    based on the flask WT Form FlaskForm class.
+    '''
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    '''
+    Create a user registration form class
+    based on the flask WT Form FlaskForm class.
+    '''
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
