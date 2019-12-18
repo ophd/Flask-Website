69ad81c87017b63cf69d96acad84151c1b1e1989
Olivier Daigle <ophd@pm.me>
2019-12-17 19:37:29 -0500

    Finished Part 6 of Tutorial
    -Added user authentication
    -User registration now uses db
    -Added logged in behaviour

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,19 +1,28 @@
-from flaskblog import db
+from flaskblog import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     '''
     A database table containing user information.
     '''
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f'User({self.username}","{self.email}", "{self.image_file}")'
+
 
 class Post(db.Model):
     '''
@@ -23,7 +32,8 @@ class Post(db.Model):
     '''
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
