dae0a0624fcbad1381372ca4051c84f96f9f3af4
Olivier Daigle <ophd@pm.me>
2019-12-22 18:18:19 -0500

    Completed Part 10 of the Flask tutorial
    - added password reset_request & reset_token forms & routes
    - added capability to send password reset email

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,10 +3,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['MAIL_SERVER'] = os.environ['MAIL_SERVER']
+app.config['MAIL_PORT'] = os.environ['MAIL_PORT']
+app.config['MAIL_USE_TLS'] = True#bool(os.environ['MAIL_USE_TLS'])
+app.config['MAIL_USERNAME'] = os.environ['EMAIL_USER']
+app.config['MAIL_PASSWORD'] = os.environ['EMAIL_PASS']
+mail = Mail(app)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
